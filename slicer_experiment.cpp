f452c157c3c7e66481d755938e62cbfa1c91b895
YichenSheng <451718145@qq.com>
2019-12-11 19:22:23 -0500

    Update experiments.

--- slicer_experiment.cpp
+++ slicer_experiment.cpp
@@ -11,58 +11,50 @@ slicer_experiment::~slicer_experiment() {
 }
 
 /* Test line segments speed */
-std::vector<layer_paths> slicer_experiment::exp_speed_test() {
-	float radius = 10.0f;
-	int tessellation_min = 4, tessellation_max = 104;
-	int tessellation_step = 10;
-	int grid_num = 4;
+std::vector<layer_paths> slicer_experiment::exp_speed_test(pd::deg turning_alpha, int seg_num) {
+	std::vector<layer_paths> ret;
 
-	auto scale_factor = [](float radius, int tessellation_num) {
-		float perimeter = 2.0 * pd::pi * radius;
-		pd::deg delta_angle = 360 / tessellation_num;
-		vec3 c = vec3(radius, 0.0f, 0.0f);
-		mat4 rot = glm::rotate(deg2rad(delta_angle), vec3(0.0f, 1.0f, 0.0f));
-		vec3 b = vec3(rot * vec4(c,0.0f));
-		vec3 a = b - c;
+	vec3 speed_vector(0.0f, 0.0f, -1.0f), axis(0.0f);
 
-		float delta_length = glm::length(a);
-		float delta_arc = deg2rad(delta_angle) * radius;
-		return delta_arc / delta_length;
+	glm::mat4 left_rot = glm::rotate(deg2rad(turning_alpha), glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 right_rot = glm::rotate(-deg2rad(turning_alpha), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	//------- Construct One Column Results --------//
+	auto building_block = [](glm::mat4 left_rot, glm::mat4 right_rot, int seg_num, vec3 speed_vector) {
+		vec3 cur_pos(0.0f);
+		closed_poly cur_layer;
+		for (int seg_i = 0; seg_i < seg_num; ++seg_i) {
+			bool is_left = (seg_i % 2 == 0);
+			speed_vector = is_left ?
+				vec3(left_rot * vec4(speed_vector, 0.0)) :
+				vec3(right_rot * vec4(speed_vector, 0.0));
+
+			vec3 head = cur_pos;
+			cur_pos = cur_pos + speed_vector;
+			vec3 tail = cur_pos;
+			cur_layer.push_back(std::make_shared<line_segment>(head, tail));
+		}
+		return cur_layer;
 	};
 
-	std::vector<layer_paths> ret;
-	for(int tes_num = tessellation_min; tes_num <= tessellation_max; tes_num += tessellation_step) {
-		std::vector<std::shared_ptr<line_segment>> boundaries;
+	closed_poly col_result = building_block(left_rot, right_rot, seg_num, speed_vector);
+	vec3 diag = col_result[col_result.size() - 1]->t;
+	vec3 offset = glm::normalize(vec3(glm::rotate(deg2rad(90.0f), vec3(0.0f, 1.0f, 0.0f)) * diag));
 
-		float fract = scale_factor(radius, tes_num);
-		INFO("Tes num: " + std::to_string(tes_num));
-		INFO("Tes scale: " + std::to_string(fract));
-		
-		vec3 lst_vec = vec3(radius, 0.0f, 0.0f) * fract;
-		vec3 cur_vec = vec3(radius, 0.0f, 0.0f) * fract;
-		pd::deg delta_angle = 360.0 / tes_num;
-		for(int i = 0; i < tes_num; ++i) {
-			mat4 rot = glm::rotate(deg2rad(delta_angle), glm::vec3(0.0f, 1.0f, 0.0f));
-			cur_vec = vec3(rot * vec4(cur_vec, 0.0f));
-			
-			boundaries.push_back(std::make_shared<line_segment>(lst_vec, cur_vec));
-			lst_vec = cur_vec;
+	int col_num = 10;
+	layer_paths one_layer;
+	for(int j =0;j < col_num; ++j) {
+		closed_poly cur_col;
+
+		for(auto s:col_result) {
+			cur_col.push_back(std::make_shared<line_segment>(
+				s->t + offset * (float)j, 
+				s->h + offset * (float)j));
 		}
 
-		layer_paths cur_layer;
-		// copy paste to a gird 
-		float offset = 2.5 * radius;
-		for (int xi = 0; xi < grid_num; ++xi) for (int yi = 0; yi < grid_num; ++yi) {
-			closed_poly cur_poly;
-			vec3 offset_vector = vec3(offset * xi, 0.0f, offset * yi);
-			for(auto b:boundaries) {
-				cur_poly.push_back(std::make_shared<line_segment>(b->t + offset_vector, b->h + offset_vector));
-			}
-			cur_layer.push_back(cur_poly);
-		}
-
-		ret.push_back(cur_layer);
+		one_layer.push_back(cur_col);
 	}
 
+	ret.push_back({ one_layer });
 	return ret;
 }
--- slicer_experiment.h
+++ slicer_experiment.h
@@ -8,6 +8,6 @@ public:
 	slicer_experiment();
 	~slicer_experiment();
 
-	static std::vector<layer_paths> exp_speed_test();
+	static std::vector<layer_paths> exp_speed_test(pd::deg turning_alpha, int seg_num);
 };
 
