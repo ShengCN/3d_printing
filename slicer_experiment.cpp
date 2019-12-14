82c1ea7905ed6b0b827af78213baf37fb3842062
YichenSheng <451718145@qq.com>
2019-12-13 21:59:19 -0500

    Update experiment.

--- slicer_experiment.cpp
+++ slicer_experiment.cpp
@@ -1,7 +1,7 @@
 #include "slicer_experiment.h"
 #include "geometry_ds.h"
 #include "graphics_lib/common.h"
-
+#include "graphics_lib/Render/mesh.h"
 
 slicer_experiment::slicer_experiment() {
 }
@@ -37,23 +37,46 @@ std::vector<layer_paths> slicer_experiment::exp_speed_test(pd::deg turning_alpha
 		return cur_layer;
 	};
 
+	if (seg_num % 2 == 0)
+		seg_num = seg_num - 1;
+
 	closed_poly col_result = building_block(left_rot, right_rot, seg_num, speed_vector);
 	vec3 diag = col_result[col_result.size() - 1]->t;
-	vec3 offset = glm::normalize(vec3(glm::rotate(deg2rad(90.0f), vec3(0.0f, 1.0f, 0.0f)) * diag));
+	
+	vec3 col_rot_axis, offset;
+	if(col_result.size() >= 2) {
+		col_rot_axis = col_result[1]->h - col_result[0]->t;
+		vec3 a = col_result[0]->t, b = col_result[0]->h, c = col_result[1]->h;
+		offset = (2.0f * b - a - c);
+	} else {
+		WARN("line segment num is too small");
+		assert(false);
+	}
 
-	int col_num = 10;
+	 mat4 col_rot_mat = glm::rotate(deg2rad(180.0), col_rot_axis);
+	// rotate the first building block 
+	closed_poly col_double_result = col_result;
+	for(auto s: col_result) {
+		col_double_result.push_back(
+			std::make_shared<line_segment>(
+			vec3(col_rot_mat * vec4(s->t, 0.0)),
+			vec3(col_rot_mat * vec4(s->h, 0.0))));
+	}
+
 	layer_paths one_layer;
-	for(int j =0;j < col_num; ++j) {
+	int col_num =  50;
+	for(int col_i = 0; col_i < col_num; ++col_i) {
 		closed_poly cur_col;
 
-		for(auto s:col_result) {
+		for(auto s:col_double_result) {
 			cur_col.push_back(std::make_shared<line_segment>(
-				s->t + offset * (float)j, 
-				s->h + offset * (float)j));
+				s->t - offset * (float)col_i,
+				s->h - offset * (float)col_i));
 		}
 
 		one_layer.push_back(cur_col);
 	}
+
 
 	ret.push_back({ one_layer });
 	return ret;
